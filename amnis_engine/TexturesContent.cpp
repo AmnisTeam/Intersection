de9a1a294e617581155f0537287ea32a263fcc48
KoTeuKaSeeker <emaillit8@gmail.com>
2022-08-08 20:09:37 +0400

    Kek

--- amnis_engine/TexturesContent.cpp
+++ amnis_engine/TexturesContent.cpp
@@ -7,8 +7,8 @@ TexturesContent::TexturesContent(Graphics* graphics)
 {
 	this->graphics = graphics;
 	textureSky = new Texture(graphics, L"Textures//texturify_pano-1-2.jpg");
-	stoneWallNormalMap = new Texture(graphics, L"Wall_Stone_017_Normal.jpg");
-	stoneWallAlbedo = new Texture(graphics, L"Wall_Stone_017_BaseColor.jpg");
+	stoneWallNormalMap = new Texture(graphics, L"Textures\\Wall_Stone_017_Normal.jpg");
+	stoneWallAlbedo = new Texture(graphics, L"Textures\\Wall_Stone_017_BaseColor.jpg");
 	flatNormalMap = new Texture(graphics, L"Textures\\14015-normal.jpg");
 	wheelRobotBaseColor = new Texture(graphics, L"Models\\wheeled-robot\\textures\\DefaultMaterial_Base_color.png");
 	wheelRobotNormal = new Texture(graphics, L"Models\\wheeled-robot\\textures\\DefaultMaterial_Normal_OpenGL.png");
